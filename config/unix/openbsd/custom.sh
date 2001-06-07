04a76560ec88112f4330e0f28ef081af7c8cbf35
gmt <>
2001-06-07 16:48:11 +0000

    More changes based on feedback from Marc's OpenBSD testing.

--- config/unix/openbsd/custom.sh
+++ config/unix/openbsd/custom.sh
@@ -6,7 +6,7 @@ SRC=../../../src
 ARCH=`uname -m`
 
 if [ -f rswitch.$ARCH ]; then
-	cp rswitch.$ARCH $SRC/common/rswitch.c
+	cp -f rswitch.$ARCH $SRC/common/rswitch.c
 else
 	echo "#define NoCoexpr" >>$SRC/h/define.h
 fi
--- config/unix/openbsd/rswitch.c
+++ config/unix/openbsd/rswitch.c
@@ -2,11 +2,10 @@
  *  This is a dummy co-expression context switch that can be used in
  *  the absence of a working one.
  */  
-#include "../h/rt.h"
 
 int coswitch(old_cs, new_cs, fnc)
-word *old_cs, *new_cs;
-continuation fnc;
+int *old_cs, *new_cs;
+int fnc;
    {
-   err_msg(401, NULL);
+   err_msg(401, (void*)0);
    }
