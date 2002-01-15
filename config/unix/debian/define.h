4feec20272212163fc399d5bd240c0ff2d811b5c
gmt <>
2002-01-15 23:08:41 +0000

    Add configurations for Debian Linux and GNU/Hurd from Christian Hudon.

---
+++ config/unix/debian/define.h
@@ -0,0 +1,9 @@
+/*  
+ * Icon configuration file for Linux v2
+ */
+
+#define UNIX 1
+#define LoadFunc
+
+#define CComp "gcc"
+#define COpts "-O2 -fomit-frame-pointer"
---
+++ config/unix/intel_hurd/define.h
@@ -0,0 +1,9 @@
+/*
+ * Icon configuration file for the GNU Hurd system
+ */
+
+#define UNIX 1
+#define LoadFunc
+
+#define CComp "gcc"
+#define COpts "-O2 -fomit-frame-pointer"
