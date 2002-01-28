03ced9f603fa6d51af947afa13bd30ca541c3417
gmt <>
2002-01-28 18:27:09 +0000

    Add pthreads_freebsd configuration.

---
+++ config/unix/pthreads_freebsd/define.h
@@ -0,0 +1,13 @@
+/*  
+ * Icon configuration file for FreeBSD using POSIX threads
+ */
+
+#define UNIX 1
+#define GenericBSD
+#define BSD_4_4_LITE	1	/* This is new, for 4.4Lite specific stuff */
+
+#define LoadFunc
+#define ExecImages
+
+#define CComp "gcc"
+#define COpts "-O2"
