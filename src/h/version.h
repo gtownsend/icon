27ec395695cb93e131a180711f8fc2c373d49c1e
gmt <>
2003-04-15 20:50:20 +0000

    Update &version to 9.4.2 (April 16, 2003).

--- src/h/version.h
+++ src/h/version.h
@@ -11,8 +11,8 @@
  *  Icon version number and date.
  *  These are the only two entries that change any more.
  */
-#define VersionNumber "pre942c"
-#define VersionDate "Spring, 2003"
+#define VersionNumber "9.4.2"
+#define VersionDate "April 16, 2003"
 
 /*
  * Version number to insure format of data base matches version of iconc
