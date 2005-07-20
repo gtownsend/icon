42f42ddaea4ae155b8930463f57b34884e604b0c
gmt <>
2005-07-20 21:11:14 +0000

    Consolidate and rename HP-UX configurations.

--- config/hp_risc/define.h
+++
@@ -1,7 +0,0 @@
-/*
- *  Icon configuration file for HP Precision [RISC] architecture
- */
-#define UNIX 1
-
-#define CStateSize 20
-#define StackSize 10000
--- config/hp_risc_gcc/define.h
+++
@@ -1,7 +0,0 @@
-/*
- *  Icon configuration file for HP Precision [RISC] architecture using gcc
- */
-#define UNIX 1
-
-#define CStateSize 20
-#define StackSize 10000
