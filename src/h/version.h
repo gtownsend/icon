c0c9569e67d451578227adf9889252b6c57b8f04
gmt <>
2008-03-09 14:54:14 +0000

    Set version string to:  Version 9.5.cvs, Spring 2008.

--- src/h/version.h
+++ src/h/version.h
@@ -11,8 +11,8 @@
  *  Icon version number and date.
  *  These are the only two entries that change any more.
  */
-#define VersionNumber "9.5.a"
-#define VersionDate "Autumn, 2007"
+#define VersionNumber "9.5.cvs"
+#define VersionDate "Spring 2008"
 
 /*
  * Version number to insure format of data base matches version of iconc
