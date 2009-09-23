5285ee5cf6d049f3fb44ff58911734d7c171f2d2
gmt <>
2009-09-23 17:54:33 +0000

    Update VersionDate to "Fall 2009".

--- src/h/version.h
+++ src/h/version.h
@@ -12,7 +12,7 @@
  *  These are the only two entries that change any more.
  */
 #define VersionNumber "9.5.cvs"
-#define VersionDate "Summer 2009"
+#define VersionDate "Fall 2009"
 
 /*
  * Version number to insure format of data base matches version of iconc
