cbb9168ab17de7aec8d8b014252b7fd1fda6ab84
gmt <>
2010-03-25 15:54:08 +0000

    Best-guess cleanup of issues with contributed code.

--- ipl/packs/icondb/postgresql/makeso.sh
+++
@@ -1,3 +0,0 @@
-#!/opt/gnu/bin/bash
-CFG=pg_config
-bash -c "gcc -shared -o pgsqldb.so -fPIC -I../../../cfuncs -I`$CFG --includedir` pgsqldb.c -L`$CFG --libdir` -R`$CFG --libdir` -L/soft/ssl/openssl097-current/lib -R/soft/ssl/openssl097-current/lib -lpq -lssl -lcrypto"
