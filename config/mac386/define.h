62b364867b92690917837edea3b5286c3b583db0
gmt <>
2010-03-26 16:10:45 +0000

    Add mac386 configuration.

---
+++ config/mac386/define.h
@@ -0,0 +1,9 @@
+/*
+ * Icon configuration file for Macintosh
+ */
+
+#define UNIX 1
+#define MACINTOSH 1
+#define LoadFunc 
+
+#define NamedSemaphores     /* unnamed sempahores not implemented by OS 10.6 */
