d3ec12c1db25574d7182bc40623d9e28be926db4
gmt <>
2010-04-13 00:18:34 +0000

    Set version number in CVS to 9.5.cvs.

--- src/h/version.h
+++ src/h/version.h
@@ -11,8 +11,8 @@
  *  Icon version number and date.
  *  These are the only two entries that change any more.
  */
-#define VersionNumber "9.5.0"
-#define VersionDate "April 12, 2010"
+#define VersionNumber "9.5.cvs"
+#define VersionDate "April 13, 2010"
 
 /*
  * Version number to insure format of data base matches version of iconc
