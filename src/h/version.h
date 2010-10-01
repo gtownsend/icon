18eeb9b1f6325309f79a4dce28864837d092227d
gmt <>
2010-10-01 19:34:49 +0000

    Note validation under Mac Xcode 3.2.4 / Snow Leopard 10.6.4.

--- src/h/version.h
+++ src/h/version.h
@@ -12,7 +12,7 @@
  *  These are the only two entries that change any more.
  */
 #define VersionNumber "9.5.cvs"
-#define VersionDate "May 21, 2010"
+#define VersionDate "October 1, 2010"
 
 /*
  * Version number to insure format of data base matches version of iconc
