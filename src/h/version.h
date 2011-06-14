2ba1b43fa6ad13671eb5c77e46b724fdc4cf29eb
gmt <>
2011-06-14 20:18:54 +0000

    Note successful build under Macintosh XCode 4.0.2.

--- src/h/version.h
+++ src/h/version.h
@@ -12,7 +12,7 @@
  *  These are the only two entries that change any more.
  */
 #define VersionNumber "9.5.cvs"
-#define VersionDate "June 12, 2011"
+#define VersionDate "June 14, 2011"
 
 /*
  * Version number to insure format of data base matches version of iconc
