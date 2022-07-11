bfb571cf481a63705cbf753c9c2a6356ed464651
Gregg Townsend <gmt@cs.arizona.edu>
2022-07-10 19:54:17 -0700

    Don't copy atop existing executable (fix MacOS Montery "Killed 9" problem).

--- src/h/version.h
+++ src/h/version.h
@@ -11,7 +11,7 @@
  *  Icon version number and date.
  *  These are the only two entries that change any more.
  */
-#define VersionNumber "9.5.22b"
+#define VersionNumber "9.5.22c"
 #define VersionDate "July 10, 2022"
 
 /*
